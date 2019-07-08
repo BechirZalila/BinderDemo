2f0ee3a6b556a0c33eaee06d454d28083f4fa1bf
Bechir Zalila <bechir.zalila@gmail.com>
2019-07-08 20:39:57 +0100

    * External app

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := binder
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES) # IMPORTANT!!!
 LOCAL_SRC_FILES := binder.cpp
 LOCAL_SHARED_LIBRARIES := libutils libcutils libbinder liblog
 LOCAL_C_INCLUDES += frameworks/native/include system/core/include
